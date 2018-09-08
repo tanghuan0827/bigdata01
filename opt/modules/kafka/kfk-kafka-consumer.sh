3de110e50d5acda1a475f06314455672aca42cf6
tanghuan0827 <1255196919@qq.com>
2018-09-07 23:54:27 -0400

    Revert "#"
    This reverts commit 9495ed3b3d47e34f9b0365bdb34f3d17a0a5d55d.
    
    Conflicts:
    	opt/modules/kafka/kfk-kafka-consumer.sh

--- opt/modules/kafka/kfk-kafka-consumer.sh
+++ opt/modules/kafka/kfk-kafka-consumer.sh
@@ -1,3 +1,10 @@
+<<<<<<< HEAD
 #!/bin/bash
 
 bin/kafka-console-consumer.sh --zookeeper bigdata-pro01.kfk.com:2181,bigdata-pro02.kfk.com:2181,bigdata-pro03.kfk.com:2181 --topic weblogs --from-beginning
+=======
+echo "kfk-kafka-consumer.sh start ......"
+bin/kfk-kafka-consumer.sh --create --zookeeper bigdata-pro01.kfk.com:2181,bigdata-pro02.kfk.com:2181,bigdata-pro03.kfk.com:2181 --from-beginning --topic test
+
+
+>>>>>>> parent of 9495ed3... #
